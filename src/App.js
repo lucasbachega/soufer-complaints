d4e5838a589771a286da12111c49d922dfc828f9
Lucas Bachega <lucas.cbachega@gmail.com>
2024-04-12 18:08:40 -0300

    add loading app index html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssVarsProvider } from "@mui/joy/styles";
+import "./App.css";
+import { theme } from "./theme";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <CssVarsProvider theme={theme} defaultMode="light"></CssVarsProvider>;
 }
 
 export default App;
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@mui/joy/styles";
+
+export const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {},
+    },
+  },
+});
