8af303b85b4c38468c88383fcf74089b109bf2a4
Matheus <matheus.bachega@oxfield.com.br>
2024-05-01 11:44:52 -0300

    CRUD básico das configurações pt2

--- backend/routes/complaints.js
+++ backend/routes/complaints.js
@@ -45,4 +45,46 @@ router.get("/setor", async (req, res) => {
   return res.send(r);
 });
 
+/**
+ * Listar Produtos disponíveis para seleção
+ */
+router.get("/produtos", async (req, res) => {
+  const r = await Database.collection("produtos")
+    .find(
+      {
+        active: true,
+      },
+      {
+        projection: {
+          _id: 1,
+          text: 1,
+        },
+      }
+    )
+    .sort({ text: 1 })
+    .toArray();
+  return res.send(r);
+});
+
+/**
+ * Listar Categorias disponíveis para seleção
+ */
+router.get("/categorias", async (req, res) => {
+  const r = await Database.collection("categorias")
+    .find(
+      {
+        active: true,
+      },
+      {
+        projection: {
+          _id: 1,
+          text: 1,
+        },
+      }
+    )
+    .sort({ text: 1 })
+    .toArray();
+  return res.send(r);
+});
+
 module.exports = router;
